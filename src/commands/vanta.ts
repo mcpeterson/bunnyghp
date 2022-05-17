014fd17d5eb5d5607950f8a61e53ffa7c18e8341
Jake (Code Machine) Peterson <jacobdean@gmail.com>
2022-05-17 16:57:25 -0700

    adds opportunity

--- src/commands/vanta.ts
+++ src/commands/vanta.ts
@@ -9,4 +9,11 @@ export const vanta: () => CommandDefinition[] = () => [
     example: 'v Vanta Home Page',
     desc: 'Just go to the Vanta App Home page.',
   },
+  {
+    template: `opp${QUERY}`,
+    toUrl: ({ query }) =>
+      `https://vanta.lightning.force.com/lightning/r/Opportunity/${query ? `${query}/view` : ''}`,
+    example: 'opp ABCDEFGHI',
+    desc: 'Takes you to a specific opportunity in sfdc'
+  },  
 ];
